1cccac47f7afae0edbb8979d0e4fd6c2f4ec63e4
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-02-23 17:22:45 -0300

    allow to load configuration from external file

--- src/control_chain.h
+++ src/control_chain.h
@@ -19,6 +19,10 @@ extern "C"
 #include "update.h"
 #include "msg.h"
 
+#if __has_include("config.h")
+#include "config.h"
+#endif
+
 
 /*
 ****************************************************************************************************
@@ -36,12 +40,14 @@ extern "C"
 ****************************************************************************************************
 */
 
+#ifndef CC_USE_EXTERNAL_CONFIG
 // maximum number of devices that can created
 #define CC_MAX_DEVICES      1
 // maximum number of actuators that can created per device
-#define CC_MAX_ACTUATORS    4
+#define CC_MAX_ACTUATORS    1
 // maximum number of assignments that can created per actuator
 #define CC_MAX_ASSIGNMENTS  1
+#endif
 
 
 /*
