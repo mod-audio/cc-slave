c2b39818cc33ba2cc240a48e119389f7f3f9705f
John Whitmore <johnfwhitmore@gmail.com>
2020-09-14 19:28:55 +0100

    Changed C99 code to compile with XC16 compiler

--- src/core.c
+++ src/core.c
@@ -74,6 +74,9 @@ static cc_handle_t g_cc_handle;
 
 static void send(cc_handle_t *handle, const cc_msg_t *msg)
 {
+#ifdef XC16
+    uint32_t j;
+#endif
     uint8_t *buffer = handle->msg_tx->header;
 
     // header
@@ -88,7 +91,11 @@ static void send(cc_handle_t *handle, const cc_msg_t *msg)
     {
         if (msg != handle->msg_tx)
         {
+#ifdef XC16
+            for (j = 0; j < msg->data_size; j++)
+#else
             for (uint32_t j = 0; j < msg->data_size; j++)
+#endif
                 buffer[i++] = msg->data[j];
         }
         else
