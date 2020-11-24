ebbaf77590d5d001f688c890f91e0aaa48358043
Adam <adam.buijks@gmail.com>
2020-11-24 10:47:51 +0100

    fixed review issues

--- src/actuator.c
+++ src/actuator.c
@@ -112,7 +112,7 @@ static int momentary_process(cc_actuator_t *actuator, cc_assignment_t *assignmen
             }
 
             // toggle mode
-            if (assignment->mode & CC_MODE_TOGGLE)
+            else if (assignment->mode & CC_MODE_TOGGLE)
             {
                 assignment->value = 1.0 - assignment->value;
             }
